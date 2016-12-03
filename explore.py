c59577f55614a85a226967b349e2c3b7066cf667
Tianxi Ku <kutianxi@outlook.com>
2016-12-03 17:14:20 +0800

    say something when program is finished due to certain reasons

--- explore.py
+++ explore.py
@@ -1,5 +1,7 @@
 #!/usr/local/bin/ python
+# coding=utf-8
 from function import *
+import os
 
 times = raw_input('times=')
 chapter = raw_input('chapter=')
@@ -12,8 +14,10 @@ for num in range(int(times)):
     task.get_small_box()
     task.get_big_box()
     if task.found_shi_ju():
+        os.system('say -v Ting-Ting "找到石距啦"')
         break
     if task.is_pl_not_enough():
+        os.system('say -v Ting-Ting "体力不足"')
         break
     task.analysis()
 task.analysis()
