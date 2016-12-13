4d8619e787e3da1a4f9fd8d28cb2c0ebca68ef84
YingZhou <yingzhou@thoughtworks.com>
2016-12-13 11:58:58 +0800

    improve input for group

--- group.py
+++ group.py
@@ -1,20 +1,46 @@
 #!/usr/local/bin/ python
 from function import *
+import sys
+import getopt
 
-times = raw_input('times=')
-is_lead = raw_input('is_lead=')
 
-task = Group()
+def main(argv):
+    times = 0
+    is_lead = ''
+    device = ''
 
-for num in range(int(times)):
-    if is_lead == '1':
-        if not task.start_group_fight():
-            break
-        task.group_fight()
-        task.click_ok()
-    else:
-        if not task.wait_in_group():
-            break
-        task.group_fight()
-        task.click_ok()
-    task.analysis()
+    try:
+        opts, args = getopt.getopt(argv, "ht:l:d:", ["times=", "is_lead=", "device="])
+    except getopt.GetoptError:
+        print 'Error: explore.py -t <times>(int) -l <is_lead>(y or n) -d <device>(android or ios)'
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt == "-h":
+            print 'Error: explore.py -t <times>(int) -l <is_lead>(y or n) -d <device>(android or ios)'
+            sys.exit()
+        elif opt in ("-t", "--times"):
+            times = int(arg)
+        elif opt in ("-l", "--is_lead"):
+            is_lead = arg
+        elif opt in ("-d", "--device"):
+            device = arg
+
+    task = Group(device)
+
+    for num in range(int(times)):
+        if is_lead == 'y':
+            if not task.start_group_fight():
+                break
+            task.group_fight()
+            task.click_ok()
+        else:
+            if not task.wait_in_group():
+                break
+            task.group_fight()
+            task.click_ok()
+        task.analysis()
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
