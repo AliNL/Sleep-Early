212c39ea22d39b145f8f8ccbebc808da0e958621
YingZhou <yingzhou@thoughtworks.com>
2016-12-13 11:52:59 +0800

    improve input for break

--- break.py
+++ break.py
@@ -1,9 +1,35 @@
 #!/usr/bin/env python
 from function import *
+import sys
+import getopt
 
-times = raw_input('times=')
-level = raw_input('level/10=')
 
-task = Break(int(times), int(level))
+def main(argv):
+    times = 0
+    level = 0
+    device = ''
 
-task.breaking()
+    try:
+        opts, args = getopt.getopt(argv, "ht:l:d:", ["times=", "level=", "device="])
+    except getopt.GetoptError:
+        print 'Error: explore.py -t <times>(int) -l <level/10>(0<int<8) -d <device>(android or ios)'
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt == "-h":
+            print 'Error: explore.py -t <times>(int) -l <level/10>(0<int<8) -d <device>(android or ios)'
+            sys.exit()
+        elif opt in ("-t", "--times"):
+            times = int(arg)
+        elif opt in ("-l", "--level"):
+            chapter = int(arg)
+        elif opt in ("-d", "--device"):
+            device = arg
+
+    task = Break(times, level, device)
+
+    task.breaking()
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
