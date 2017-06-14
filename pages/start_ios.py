4ecccf78630f0f30c0533b07ad95059ba6f17ca9
Ying Zhou <yingzhou@thoughtworks.com>
2017-06-14 14:47:50 +0800

    remove start_ios

--- pages/start_ios.py
+++
@@ -1,8 +0,0 @@
-# coding=utf-8
-import atx
-
-driver = atx.connect('http://localhost:8100')
-sid = str(driver.start_app('com.netease.onmyoji'))
-fl = open('session_id_ios', 'wb')
-fl.write(sid[16:52])
-fl.close()
